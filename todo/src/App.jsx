987aa4a648991ded42b9bb7e3f9033474a2ef820
dev-rafa1707 <rafa1707@gmail.com>
2023-11-22 20:54:59 -0300

    add basic form structure

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,5 +1,25 @@
 import './styles.css'
 
 export default function App() {
-  return "Hello World."
+  return  (
+    <>
+      <form className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input id='item' type='text' />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h1>Todo List</h1>
+      <ul>
+        <li>
+          <label>
+            <input type='checkbox' />
+            Item 1
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
